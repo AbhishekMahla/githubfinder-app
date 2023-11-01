4a335c35f660dfb7ac47b684d47d4c4a4135dfde
AbhishekMahla <abhishekmahla22@gmail.com>
2023-11-01 13:12:58 +0530

    Final Commit

--- src/context/github/githubContext.js
+++ src/context/github/githubContext.js
@@ -2,7 +2,6 @@ import { createContext, useReducer } from "react";
 import githubReducers from "./GithubReducers";
 
 const GithubContext = createContext();
-const GITHUB_URL = "https://api.github.com";
 
 export const GithubProvider = ({ children }) => {
   const intialState = {
@@ -14,88 +13,21 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducers, intialState);
 
-  const fetchUsers = async () => {
-    setLoading();
-    const response = await fetch(`${GITHUB_URL}/users`);
-    const data = await response.json();
+  // const fetchUsers = async () => {
+  //   const response = await fetch(`${GITHUB_URL}/users`);
+  //   const data = await response.json();
 
-    dispatch({
-      type: "GET_USERS",
-      payload: data,
-    });
-  };
-
-  // Search User
-  const searchUsers = async (text) => {
-    setLoading();
-    const params = new URLSearchParams({
-      q: text,
-    });
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`);
-    const { items } = await response.json();
-
-    dispatch({
-      type: "GET_USERS",
-      payload: items,
-    });
-  };
-
-  // Get Single User
-  const getUser = async (login) => {
-    setLoading();
-
-    const response = await fetch(`${GITHUB_URL}/users/${login}`);
-
-    if (response.status === 404) {
-      window.location = "./notfound";
-    } else {
-      const data = await response.json();
-      dispatch({
-        type: "GET_USER",
-        payload: data,
-      });
-    }
-  };
-
-  // Get User Repos
-  const getUserRepos = async (login) => {
-    setLoading();
-    const params = new URLSearchParams({
-      sort: "created",
-      per_page: 10,
-    });
-
-    const response = await fetch(
-      `${GITHUB_URL}/users/${login}/repos?${params}`
-    );
-    const data = await response.json();
-
-    dispatch({
-      type: "GET_REPOS",
-      payload: data,
-    });
-  };
-  // Clear Search
-  const clearUsers = async () => {
-    dispatch({
-      type: "CLEAR_USERS",
-    });
-  };
-  //Set Loading
-  const setLoading = () => dispatch({ type: "SET_LOADING" });
+  //   dispatch({
+  //     type: "GET_USERS",
+  //     payload: data,
+  //   });
+  // };
 
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        loading: state.loading,
-        user: state.user,
-        repos: state.repos,
-        fetchUsers,
-        searchUsers,
-        clearUsers,
-        getUser,
-        getUserRepos,
+        ...state,
+        dispatch,
       }}
     >
       {children}
