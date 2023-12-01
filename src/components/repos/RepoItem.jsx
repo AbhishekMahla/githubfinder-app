93735746418c0cc78e887f45847f865d5761c31c
Abhishek Mahla <abhishekmahla@Abhisheks-MacBook-Air.local>
2023-12-01 11:25:36 +0530

    Pwa added updated style

--- src/components/repos/RepoItem.jsx
+++ src/components/repos/RepoItem.jsx
@@ -15,7 +15,7 @@ function RepoItem({ repo }) {
   return (
     <div className=" mb-2 rounded-md card bg-gray-800 hover:bg-gray-900">
       <div className="card-body">
-        <h3 className="mb-2 text-xl font-semibold">
+        <h3 className="mb-2 text-xl font-semibold" style={{ color: "white" }}>
           <a href={html_url} target="_blank" rel="noreferrer">
             <FaLink className=" inline mr-1" />
             {name}
